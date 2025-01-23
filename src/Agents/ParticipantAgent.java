9cf31ef2484282afb3a10b2a82b2dacf01975c20
Ignacio García Hernanz <166425987+ignaciogh33@users.noreply.github.com>
2025-01-23 19:08:43 +0100

    bvowv

--- src/Agents/ParticipantAgent.java
+++ src/Agents/ParticipantAgent.java
@@ -32,7 +32,7 @@ public class ParticipantAgent extends Agent {
         }
 
         // Initialize the city map
-        cityMap = CityMap.getInstance(5, 10);
+        cityMap = CityMap.getInstance(20, 100);
 
         // Store its own location in the map
         agentLocations.put(getLocalName(), location);
